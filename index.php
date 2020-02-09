4d3f6a1ccb305191d16f5df7173600573abe07b8
bobbeltje <matthiaswubs@gmail.com>
2020-02-09 17:00:12 +0100

    adding items to db

--- index.php
+++ index.php
@@ -1,11 +1,7 @@
 <?php
 require_once "protected/pdo.php";
 session_start();
-?>
-<html>
-<head>
-</head><body>
-<?php
+
 if ( isset($_SESSION['error']) ) {
     echo '<p style="color:red">'.$_SESSION['error']."</p>\n";
     unset($_SESSION['error']);
@@ -14,7 +10,31 @@ if ( isset($_SESSION['success']) ) {
     echo '<p style="color:green">'.$_SESSION['success']."</p>\n";
     unset($_SESSION['success']);
 }
+
+if ( isset($_POST['item']) && isset($_POST['category']) ) {
+    if ( $_POST['category'].'oi' === '0oi') {
+        $_SESSION['error'] = 'You need to set a category';
+        header( 'Location: index.php' ) ;
+        return;
+    }
+
+    $sql = "INSERT INTO shopping_list (item, category, quantity) 
+              VALUES (:item, :category, :quantity)";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute(array(
+        ':item' => $_POST['item'],
+        ':category' => $_POST['category'],
+        ':quantity' => $_POST['quantity']));
+   $_SESSION['success'] = 'Record Added';
+   header( 'Location: index.php' ) ;
+   return;
+}
 ?>
+<html>
+<head>
+    <?php print_r($_POST); ?>
+</head>
+<body>
 <p><b>Add something</b></p>
 <form method='post'>
     <p>Item: <input type='text' name='item'></p>
