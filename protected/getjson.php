a5431c987ad1e5c9501d075aafd6281c8a6c40a2
bobbeltje <matthiaswubs@gmail.com>
2020-02-10 22:06:43 +0100

    order table

--- protected/getjson.php
+++ protected/getjson.php
@@ -2,7 +2,8 @@
 require_once "pdo.php";
 session_start();
 header('Content-Type: application/json; charset=utf-8');
-$stmt = $pdo->query("SELECT item, category, quantity, id FROM shopping_list");
+$stmt = $pdo->query("SELECT item, category, quantity, id FROM shopping_list
+    ORDER BY category DESC");
 $rows = array();
 while ( $row = $stmt->fetch(PDO::FETCH_ASSOC) ) {
   $rows[] = $row;
